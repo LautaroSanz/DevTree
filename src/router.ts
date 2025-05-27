7f995c08546a9d2c883f59225fc0e355f186d277
Lautaro Sanz <44878724@alumnos.frsr.utn.edu.ar>
2025-05-27 10:08:14 -0300

    solicitudes con Tuhnder client

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,11 @@ import {Router} from 'express'
 
 const router=Router()
 
+/**Autenticacion y registro */
 
+router.post('/auth/register', (req,res)=>{ //usamos post para cifrado  de datos
+    console.log(req)
+
+})
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import express from 'express' //ESM ecmascript modules
 import router from './router'
 const app=express()
 
+//leer datos de formularios
+
+app.use(express.json())  //habilitamos tratamiento de jsons en solicitudes http
+
+
 app.use('/',router)
 //routing
 
