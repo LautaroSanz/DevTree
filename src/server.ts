4acc9eb5e72a8db9432e7c1088e7ff36e5404a42
Lautaro Sanz <44878724@alumnos.frsr.utn.edu.ar>
2025-05-27 11:12:00 -0300

    variables de entorno

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express' //ESM ecmascript modules
+import 'dotenv/config'
 import router from './router'
-const app=express()
+import {connectDB} from './config/db'
 
+
+const app=express()
+connectDB()
 //leer datos de formularios
 
 app.use(express.json())  //habilitamos tratamiento de jsons en solicitudes http
